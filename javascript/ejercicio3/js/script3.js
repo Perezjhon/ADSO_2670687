d52492efe6ecc71c1515927b6aa996be0fa96461
jhon alexander <perezjhon908@gmail.com>
2023-07-25 22:26:45 -0500

    realizacion de ejercicio

---
+++ javascript/ejercicio3/js/script3.js
@@ -0,0 +1,14 @@
+/* Ejercicio 1 - Una tienda ofrece un 25% de descuento por cada compra. Un cliente desea
+saber cuánto deberá pagar por su compra.*/
+
+/*let compra = prompt("escriba el valor de su compra");
+let desc = (25/100);
+let tpag = parseInt(compra*desc);
+alert("su total  de pagar es: "+tpag);*/
+
+/* Ejercicio 2 - Suponga que un individuo desea invertir su capital en un banco y desea saber
+cuánto dinero ganara después de un mes si el banco paga a razón de 2%
+mensual. */
+
+let invercion = prompt("escribir valor de invercion ")
+let razon = (2/100);
