aaa742fc49f2c7d77ddb8481aaffdcd437d011cc
jhon alexander <perezjhon908@gmail.com>
2023-07-25 21:41:18 -0500

    se realizo el ejercicio 2

---
+++ javascript/ejercicio2/js/script2.js
@@ -0,0 +1,25 @@
+/* dado el sueldo de un empleado
+ imprimir en una alerta del nuevo sueldo
+ del empleado si tuvo un aumento del 10%*/
+
+ /*const sueldo = prompt("ingrese el valor del sueldo");
+ const psueldo= 10/100;
+ const nsueldo = sueldo + parseInt(sueldo) +parseInt(sueldo*sueldo);
+ alert("el nuevo sueldo con el aumento es "+nsueldo);*/
+
+ /*Ejercicio 3 - un vendendor que tiene su sueldo base,
+  recibe una comision del 30% del total de las ventas del mes, cuanto ganara en un mes que
+  tuvo tres ventas  */
+
+/*let sueldo = prompt("ingrese el valor del sueldo ");
+let ventas1 = prompt("valor de la venta 1 ")
+let ventas2 = prompt("valor de la venta 2 ")
+let ventas3 = prompt("valor de la venta 3 ")
+let tvent = parseInt(ventas1)+parseInt(ventas2)+parseInt(ventas3);
+let pcomision = 30/100;
+let comision = parseInt(tvent*pcomision);
+let nsueldo  = parseInt(comision)+parseInt(sueldo)
+alert("las ganacias del mes " +nsueldo);*/
+
+
+
