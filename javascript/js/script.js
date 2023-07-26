c3661d25c72022e6ceca94f5287231c100654dcf
jhon alexander <perezjhon908@gmail.com>
2023-07-25 19:35:21 -0500

    se agrego la carpeta de Javascript

---
+++ javascript/js/script.js
@@ -0,0 +1,51 @@
+//console.log("Analisis y Desarrollo de Sofware")
+
+//alert("Analisis y Desarrollo de Sofware")
+
+//para comentar en una linea de codigo 
+
+//document.write("Analisis y Desarrollo de Sofware")
+
+//declar variables var
+
+//var edad =17;
+
+
+
+//const nom="jhon";
+//let num=2.5;
+//booleano=true;false
+
+/* unir 2 cadenas de texto 
+const nom="jhon"
+const ape='perez'
+alert("mi nombre es "+nom+" y mi apellido es "+ape) */
+
+/* operadores aritmeticos 
+const num1=5
+const num2=3
+const div = num1/num2
+parseInt(div)
+alert("la division  es: "+div)*/
+
+let nom=prompt("escriba nombres y apellidos")
+let nom1=prompt("digite su  edad ")
+let nom2=prompt("digite su peso ")
+let nom3=prompt("digite su direccion ")
+alert("mi nombre  es: " +nom+ ", mi edad es: " +nom1+ "Años"+  ", mi peso es: " +nom2+ "Kg"+ ", mi direccion es: " +nom3)
+
+/* mostrar alerta con nombres y apellidos 
+completos,edad, peso  y direccion */
+
+//console.log(typeof booleano);typeof es para definir el tipo de dato
+ 
+//console.log("Mi edad es:"+edad);
+//alert("Mi edad es:"+edad);
+//document.write("Mi edad es:"+edad);
+
+// simbolo "+" va funcionar para concatenar 
+
+
+//const edad2=22;
+
+//let edad3=33;
